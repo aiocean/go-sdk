1b1851668b0da446debb943f9dfb3aaeb4a8130a
nguyenvanduocit+ <nguyenvanduocit@gmail.com>
2022-08-11 04:02:19 +0700

    SDK generated

--- pixiv/v1/artwork_api.pb.validate.go
+++ pixiv/v1/artwork_api.pb.validate.go
@@ -57,38 +57,33 @@ func (m *GetArtworkResponse) validate(all bool) error {
 
 	var errors []error
 
-	for idx, item := range m.GetArtwork() {
-		_, _ = idx, item
-
-		if all {
-			switch v := interface{}(item).(type) {
-			case interface{ ValidateAll() error }:
-				if err := v.ValidateAll(); err != nil {
-					errors = append(errors, GetArtworkResponseValidationError{
-						field:  fmt.Sprintf("Artwork[%v]", idx),
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			case interface{ Validate() error }:
-				if err := v.Validate(); err != nil {
-					errors = append(errors, GetArtworkResponseValidationError{
-						field:  fmt.Sprintf("Artwork[%v]", idx),
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			}
-		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
-			if err := v.Validate(); err != nil {
-				return GetArtworkResponseValidationError{
-					field:  fmt.Sprintf("Artwork[%v]", idx),
+	if all {
+		switch v := interface{}(m.GetArtwork()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, GetArtworkResponseValidationError{
+					field:  "Artwork",
 					reason: "embedded message failed validation",
 					cause:  err,
-				}
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, GetArtworkResponseValidationError{
+					field:  "Artwork",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
 			}
 		}
-
+	} else if v, ok := interface{}(m.GetArtwork()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return GetArtworkResponseValidationError{
+				field:  "Artwork",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
 	}
 
 	if len(errors) > 0 {
