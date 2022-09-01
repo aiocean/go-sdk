3e701d3fdefe4abd8956cbf11b2935586d5b4338
nguyenvanduocit+ <nguyenvanduocit@gmail.com>
2022-09-01 18:08:27 +0700

    SDK generated

--- wibuwallpaper/v1/wallpaper.pb.validate.go
+++ wibuwallpaper/v1/wallpaper.pb.validate.go
@@ -61,12 +61,6 @@ func (m *Wallpaper) validate(all bool) error {
 
 	// no validation rules for Description
 
-	// no validation rules for TotalViews
-
-	// no validation rules for TotalLikes
-
-	// no validation rules for TotalDownloads
-
 	// no validation rules for SizeMb
 
 	if all {
@@ -231,6 +225,35 @@ func (m *Wallpaper) validate(all bool) error {
 
 	// no validation rules for Width
 
+	if all {
+		switch v := interface{}(m.GetStats()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, WallpaperValidationError{
+					field:  "Stats",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, WallpaperValidationError{
+					field:  "Stats",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetStats()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return WallpaperValidationError{
+				field:  "Stats",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return WallpaperMultiError(errors)
 	}
@@ -307,3 +330,111 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = WallpaperValidationError{}
+
+// Validate checks the field values on WallpaperStats with the rules defined in
+// the proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *WallpaperStats) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on WallpaperStats with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in WallpaperStatsMultiError,
+// or nil if none found.
+func (m *WallpaperStats) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *WallpaperStats) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for Id
+
+	// no validation rules for TotalViews
+
+	// no validation rules for TotalLikes
+
+	// no validation rules for TotalDownloads
+
+	if len(errors) > 0 {
+		return WallpaperStatsMultiError(errors)
+	}
+
+	return nil
+}
+
+// WallpaperStatsMultiError is an error wrapping multiple validation errors
+// returned by WallpaperStats.ValidateAll() if the designated constraints
+// aren't met.
+type WallpaperStatsMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m WallpaperStatsMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m WallpaperStatsMultiError) AllErrors() []error { return m }
+
+// WallpaperStatsValidationError is the validation error returned by
+// WallpaperStats.Validate if the designated constraints aren't met.
+type WallpaperStatsValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e WallpaperStatsValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e WallpaperStatsValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e WallpaperStatsValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e WallpaperStatsValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e WallpaperStatsValidationError) ErrorName() string { return "WallpaperStatsValidationError" }
+
+// Error satisfies the builtin error interface
+func (e WallpaperStatsValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sWallpaperStats.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = WallpaperStatsValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = WallpaperStatsValidationError{}
