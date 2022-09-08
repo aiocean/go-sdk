833813ba5b91337388207863539ae7d262af9d8c
Được Nguyễn Văn <duocnv@fireapps.vn>
2022-09-08 15:31:07 +0700

    WIP:

--- wibuwallpaper/v1/tag_api.pb.validate.go
+++ wibuwallpaper/v1/tag_api.pb.validate.go
@@ -35,6 +35,108 @@ var (
 	_ = sort.Sort
 )
 
+// Validate checks the field values on ListTagsFilter with the rules defined in
+// the proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *ListTagsFilter) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on ListTagsFilter with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in ListTagsFilterMultiError,
+// or nil if none found.
+func (m *ListTagsFilter) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *ListTagsFilter) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for Term
+
+	if len(errors) > 0 {
+		return ListTagsFilterMultiError(errors)
+	}
+
+	return nil
+}
+
+// ListTagsFilterMultiError is an error wrapping multiple validation errors
+// returned by ListTagsFilter.ValidateAll() if the designated constraints
+// aren't met.
+type ListTagsFilterMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m ListTagsFilterMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m ListTagsFilterMultiError) AllErrors() []error { return m }
+
+// ListTagsFilterValidationError is the validation error returned by
+// ListTagsFilter.Validate if the designated constraints aren't met.
+type ListTagsFilterValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e ListTagsFilterValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e ListTagsFilterValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e ListTagsFilterValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e ListTagsFilterValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e ListTagsFilterValidationError) ErrorName() string { return "ListTagsFilterValidationError" }
+
+// Error satisfies the builtin error interface
+func (e ListTagsFilterValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sListTagsFilter.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = ListTagsFilterValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = ListTagsFilterValidationError{}
+
 // Validate checks the field values on ListTagsRequest with the rules defined
 // in the proto definition for this message. If any rules are violated, the
 // first error encountered is returned, or nil if there are no violations.
