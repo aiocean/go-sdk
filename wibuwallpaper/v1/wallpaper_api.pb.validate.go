3a80095c78aa26f84744d900bc2253b5a4d301b0
Được Nguyễn Văn <duocnv@fireapps.vn>
2022-09-08 14:55:18 +0700

    WIP:

--- wibuwallpaper/v1/wallpaper_api.pb.validate.go
+++ wibuwallpaper/v1/wallpaper_api.pb.validate.go
@@ -270,6 +270,108 @@ var _ interface {
 	ErrorName() string
 } = GetWallpaperResponseValidationError{}
 
+// Validate checks the field values on ListWallpapersFilter with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *ListWallpapersFilter) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on ListWallpapersFilter with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// ListWallpapersFilterMultiError, or nil if none found.
+func (m *ListWallpapersFilter) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *ListWallpapersFilter) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	if len(errors) > 0 {
+		return ListWallpapersFilterMultiError(errors)
+	}
+
+	return nil
+}
+
+// ListWallpapersFilterMultiError is an error wrapping multiple validation
+// errors returned by ListWallpapersFilter.ValidateAll() if the designated
+// constraints aren't met.
+type ListWallpapersFilterMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m ListWallpapersFilterMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m ListWallpapersFilterMultiError) AllErrors() []error { return m }
+
+// ListWallpapersFilterValidationError is the validation error returned by
+// ListWallpapersFilter.Validate if the designated constraints aren't met.
+type ListWallpapersFilterValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e ListWallpapersFilterValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e ListWallpapersFilterValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e ListWallpapersFilterValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e ListWallpapersFilterValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e ListWallpapersFilterValidationError) ErrorName() string {
+	return "ListWallpapersFilterValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e ListWallpapersFilterValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sListWallpapersFilter.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = ListWallpapersFilterValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = ListWallpapersFilterValidationError{}
+
 // Validate checks the field values on ListWallpapersRequest with the rules
 // defined in the proto definition for this message. If any rules are
 // violated, the first error encountered is returned, or nil if there are no violations.
@@ -293,6 +395,35 @@ func (m *ListWallpapersRequest) validate(all bool) error {
 	var errors []error
 
 	// no validation rules for PageToken
+
+	if all {
+		switch v := interface{}(m.GetFilter()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, ListWallpapersRequestValidationError{
+					field:  "Filter",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, ListWallpapersRequestValidationError{
+					field:  "Filter",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetFilter()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return ListWallpapersRequestValidationError{
+				field:  "Filter",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
 
 	if len(errors) > 0 {
 		return ListWallpapersRequestMultiError(errors)
