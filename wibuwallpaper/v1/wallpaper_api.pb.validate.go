b5e9eeb04c71f18bc6690ec72fe2815e21e70110
Được Nguyễn Văn <duocnv@fireapps.vn>
2022-09-08 12:54:01 +0700

    WIP:

--- wibuwallpaper/v1/wallpaper_api.pb.validate.go
+++ wibuwallpaper/v1/wallpaper_api.pb.validate.go
@@ -546,40 +546,6 @@ func (m *SaveWallpaperRequest) validate(all bool) error {
 
 	// no validation rules for PublisherId
 
-	for idx, item := range m.GetTags() {
-		_, _ = idx, item
-
-		if all {
-			switch v := interface{}(item).(type) {
-			case interface{ ValidateAll() error }:
-				if err := v.ValidateAll(); err != nil {
-					errors = append(errors, SaveWallpaperRequestValidationError{
-						field:  fmt.Sprintf("Tags[%v]", idx),
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			case interface{ Validate() error }:
-				if err := v.Validate(); err != nil {
-					errors = append(errors, SaveWallpaperRequestValidationError{
-						field:  fmt.Sprintf("Tags[%v]", idx),
-						reason: "embedded message failed validation",
-						cause:  err,
-					})
-				}
-			}
-		} else if v, ok := interface{}(item).(interface{ Validate() error }); ok {
-			if err := v.Validate(); err != nil {
-				return SaveWallpaperRequestValidationError{
-					field:  fmt.Sprintf("Tags[%v]", idx),
-					reason: "embedded message failed validation",
-					cause:  err,
-				}
-			}
-		}
-
-	}
-
 	// no validation rules for SourceUrl
 
 	// no validation rules for Height
